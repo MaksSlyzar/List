116512738a33851334a96a7cc5f13a69005aabac
unknown <maks.slyzar@gmail.com>
2020-08-31 01:24:02 +0300

    asd

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const app = express();
 const mainRoutes = require("./routes/main");
-const dbInit = require("./dbInit");
 const PORT = process.env.PORT || 5000;
+const path = require('path');
+const { getAllCollections, getList, addCollection, removeCollection } = require("./dbInit");
 
 app.use(express.static(__dirname + "/public"));
-app.use("", mainRoutes);
+
+
+
+app.get("/", (req, res) => {
+    res.sendFile("public/index.html");
+});
+app.get("/ajax/collections/", async (req, res) => {
+    data = await getAllCollections();
+    await res.json(data);
+});
+app.get("/ajax/add_collection/:name", async (req, res) => {
+    addCollection(req.params.name);
+    await res.json({value: "Successfully"});
+});
+app.get("/ajax/remove_collection/:id", async (req, res) => {
+    removeCollection(req.params.id);
+    await res.json({value: "Successfully"});
+});
 
 
 app.listen(PORT, console.log("Server started"));
